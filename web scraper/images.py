34b395e707be954ecd6dd50470dd7dddba509dfa
SARTHAK1SINGH <44469087+SARTHAK1SINGH@users.noreply.github.com>
2020-06-26 23:48:34 +0530

    finally saving images opppffff....

--- web scraper/images.py
+++ web scraper/images.py
@@ -1,19 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib.request
+import random
+import os
 from io import BytesIO
 from PIL import Image
 
-search = input("enter your image search: ")
-params = {"p": search}
-r = requests.get("https://www.bing.com/images/search", params=params)
+# search = input("enter your image search: ")
+# params = {"p": search}
+url = "https://www.creativeshrimp.com/top-30-artworks-of-beeple.html"
+r = requests.get(url)
 soup = BeautifulSoup(r.text, "html.parser")
-links = soup.find_all("a", {"class": "iusc"})
+links = soup.find_all("a", {"class":"lightbox"})
 
 for items in links:
-    img_obj = requests.get(items.attrs["href"])
-    print("getting: ", items.attrs["href"])
-    title = items.attrs["href"].split("/")[-1]
+    href = items.get('href')
 
-    img = Image.open(BytesIO(img_obj.content))
-    img.save("./imagessave/" + title, img.formet)
-    print(title)
+    img_name = random.randrange(1, 500)
+    full_name = str(img_name) + '.jpg'
+
+    urllib.request.urlretrieve(href, "./imagessave/"+full_name)
+
+    print("loop break...." + href)
+
+
+    # img_obj = requests.get(items.attrs["href"])
+    # print("getting: ", items.attrs["href"])
+    # title = items.attrs["href"].split("/")[-1]
+    #
+    # img = Image.open(BytesIO(img_obj.content))
+    # img.save("./imagessave/" + title, img.formet)
+    # print(title)
